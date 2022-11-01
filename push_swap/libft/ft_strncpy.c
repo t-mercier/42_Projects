21ba2b1b5bbef653e0df53d33534b26863b1812c
timothee <94700601+t-mercier@users.noreply.github.com>
2022-11-01 16:24:03 +0100

    2022-11-01 16:24:03

--- push_swap/libft/ft_strncpy.c
+++ push_swap/libft/ft_strncpy.c
@@ -12,18 +12,18 @@
 
 #include "libft.h"
 
-char	*ft_strncpy(char *dst, const char *src, size_t n)
+char	*ft_strncpy(char *dst, char *src, size_t n)
 {
-	size_t	i;
+    size_t	i;
 
-	if (!src)
-		return (0);
-	i = 0;
-	while (src[i] && i < n)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (dst);
+    if (!src)
+        return (0);
+    i = 0;
+    while (*(src + i) && i < n)
+    {
+        *(dst + i) = *(src + i);
+        i++;
+    }
+    *(dst + i) = '\0';
+    return (dst);
 }
