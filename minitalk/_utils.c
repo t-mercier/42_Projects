d1cf6c4898662fae1f2fccea880bd0fb32a23e2c
timothee <94700601+t-mercier@users.noreply.github.com>
2022-11-19 19:56:55 +0100

    2022-11-19 19:56:55

--- minitalk/_utils.c
+++ minitalk/_utils.c
@@ -1,17 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   minitalk.h                                         :+:    :+:            */
+/*   _utils.c                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: tmercier <tmercier@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/11/18 20:45:57 by tmercier      #+#    #+#                 */
-/*   Updated: 2022/11/18 20:45:57 by tmercier      ########   odam.nl         */
+/*   Created: 2022/11/19 18:44:29 by tmercier      #+#    #+#                 */
+/*   Updated: 2022/11/19 18:44:29 by tmercier      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <signal.h>
-# include "../lib42/include/lib42.h"
+#include "minitalk.h"
 
-void	init_sig();
-void	printout_pid(void);
+void	init_sig()
+{
+
+}
+
+void	printout_pid(void)
+{
+	pid_t	pid;
+	char	*sp;
+
+	pid = getpid();
+	sp = ft_itoa(pid, 10);
+	if (!sp)
+		exit_message("ERROR [ failed itoa ]", 1);
+	ft_putstr_fd("[ PID ] " , 1);
+	ft_putendl_fd(sp, 1);
+	free(sp);
+}
